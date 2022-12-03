68f14dc85b18f70db46213abd8356f8beb40b753
John Scholvin <john@scholvin.com>
2022-12-02 19:53:19 -0600

    day 2

--- src/week1.cpp
+++ src/week1.cpp
@@ -3,6 +3,7 @@
 
 #include <fstream>
 #include <set>
+#include <utility>
 
 namespace week1
 {
@@ -25,17 +26,81 @@ namespace week1
             }
 
         }
+
         auto it = meals.rbegin();
         long result = *it;
         if (part == 'a')
             return result;
         result += *++it;
         result += *++it;
+
         return result;
     }
 
-    long day01b()
+    // RPS == Rock Paper Scissors, opponent is first
+    typedef std::pair<char, char> RPS;
+    struct RPSParser
     {
-        return -1;
+        RPS operator()(const std::string& str)
+        {
+            return RPS{ str[0], str[2] };
+        }
+
+    };
+
+    long day02a()
+    {
+        std::vector<RPS> games;
+        readers::read_by_line("../data/day02.dat", RPSParser(), games);
+        long score = 0;
+        for (auto it: games)
+        {
+            long opp = it.first - 'A' + 1;
+            long me = it.second - 'X' + 1;
+
+            if (me - opp == 1 || me - opp == -2) // I win
+                score += me + 6;
+            else if (opp - me == 1 || opp - me == -2) // I lose
+                score += me;
+            else if (opp == me) // draw
+                score += me + 3;
+            else // bug
+                abort();
+        }
+
+        return score;
     }
+
+    long day02b()
+    {
+        std::vector<RPS> games;
+        readers::read_by_line("../data/day02.dat", RPSParser(), games);
+        long score = 0;
+        for (auto it: games)
+        {
+            long opp = it.first - 'A' + 1;
+            long outcome = it.second - 'X' + 1;
+            long me = 0;
+            switch (outcome)
+            {
+                case 1: /* I lose */
+                    me = opp - 1;
+                    if (me == 0) me = 3;
+                    score += me;
+                    break;
+                case 2: /* draw */
+                    me = opp;
+                    score += me + 3;
+                    break;
+                case 3: /* I win */
+                    me = opp + 1;
+                    if (me == 4) me = 1;
+                    score += me + 6;
+                    break;
+            }
+        }
+
+        return score;
+    }
+
 };
--- src/week1.h
+++ src/week1.h
@@ -5,11 +5,15 @@
 namespace week1
 {
     long day01(char part);
+    long day02a();
+    long day02b();
 
     const runner::method_map_t method_map =
     {
         { "1a", std::bind(&day01, 'a') },
         { "1b", std::bind(&day01, 'b') },
+        { "2a", std::bind(&day02a) },
+        { "2b", std::bind(&day02b) },
         //{ "4a", std::bind(&day04, 'a') },
     };
 };
